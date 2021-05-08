2f88344ec9b45da98c2dab37d4ecc6ce65491b54
Joydeep Das <joyalldeep885@gmail.com>
2021-05-09 00:51:53 +0530

    Final Commit

---
+++ Question__28.java
@@ -0,0 +1,44 @@
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Scanner;
+
+public class Question__28 {
+    public static void main(String[] args) {
+        long start=System.nanoTime();
+        Scanner Input = new Scanner(System.in);
+        System.out.println("Enter the Size and Array Elements: ");
+        int[] array = new int[Input.nextInt()];
+        for (int i = 0; i < array.length; i++) {
+            array[i] = Input.nextInt();
+        }
+        System.out.println("Enter the Triplet Sum: ");
+
+        System.out.println(Triplet(array,(Input.nextInt())));
+        long END=System.nanoTime();
+        System.out.println(END-start);
+    }
+
+    public static boolean Triplet(int[] array,int k) {
+        HashSet<Integer> Map = new HashSet<>();
+        for (int i = 0; i < array.length; i++) {
+           Map.add(array[i]);
+        }
+
+        for(int i=0;i< array.length-1;i++)
+        {
+            for(int j=i+1;j< array.length;j++)
+            {
+                Iterator<Integer> iterate=Map.iterator();
+               while(iterate.hasNext())
+               {
+                   if(array[i]+array[j]+iterate.next()==k)
+                   {
+                       return true;
+                   }
+               }
+            }
+
+        }
+        return false;
+    }
+}
